8044fe31268d4d616388726c2eadb8ec68f34aa9
mhdi-742 <mhgit7427778@gmail.com>
2024-08-12 22:50:16 +0530

    v5

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -19,7 +19,8 @@ const Home=()=>{
         if(count>0) setcount(count-1);
         else setcount(size-1);
       }
-    return(
+    if(data.length)
+    {return(
         <>
         <Navbar></Navbar>
         <div className="basiccard">
@@ -40,6 +41,30 @@ const Home=()=>{
       </div>
       </div>
       </>
-    )
+    )}
+    else {
+      return(
+        <>
+        <Navbar></Navbar>
+        <div className="basiccard">
+        <div className="scene scene--card">
+        <div className={flip}>
+          <div className="card__face card__face--front" onClick={() => Setflip('card is-flipped')}>NO QUESTION</div>
+          <div className="card__face card__face--back" onClick={() => Setflip('card')}>NO QUESTION</div>
+        </div>
+      </div>
+      <p className="reveal">Click card to REVEAL(wait for sometimes if it looks stuck)</p>
+      <div className="buttons">
+         <div className={prev}>
+           <button className="b1"onClick={async()=>{await Setflip('card');await setTimeout(()=>sub(),500);}}>PREV</button>
+         </div>
+         <div className={next}>
+           <button className="b2" onClick={async()=>{await Setflip('card'); await setTimeout(()=>add(),500);}}>NEXT</button>
+         </div>
+      </div>
+      </div>
+      </>
+      )
+    }
 }
 export default Home;
